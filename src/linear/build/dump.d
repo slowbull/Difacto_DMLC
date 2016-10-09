b2926fecf244371badfdd20e5234ef24c9b5f907
slowbull <huozhouyuan@gmail.com>
2016-10-09 01:01:37 -0500

    remove deps

--- src/linear/build/dump.d
+++
@@ -1,8 +0,0 @@
-build/dump.o: dump.cc ../../ps-lite/src/dmlc/io.h \
- ../../dmlc-core/include/dmlc/logging.h \
- ../../dmlc-core/include/dmlc/./base.h \
- ../../ps-lite/deps/include/glog/logging.h \
- ../../ps-lite/deps/include/gflags/gflags.h \
- ../../ps-lite/deps/include/gflags/gflags_declare.h \
- ../../ps-lite/deps/include/glog/log_severity.h \
- ../../ps-lite/deps/include/glog/vlog_is_on.h
